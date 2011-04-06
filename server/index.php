2ccf99dd48d18ef3b70088576c425982fd9aa867
Lars Larsson <lars.la@gmail.com>
2011-04-06 16:25:37 +0200

    Fixed some bugs

--- server/index.php
+++ server/index.php
@@ -3,14 +3,15 @@
 require_once("config.php");
 require_once("php_include/db.php");
 
-$db = new db($db_server, $db_user, $db_pass, $db_name);
+$db = new database($db_server, $db_user, $db_pass, $db_name);
 
 $db->query("SELECT * FROM sunray WHERE `hidden` != 1 ORDER BY `token` ASC");
 
 foreach($db->getrows() as $row) {
 	echo "token=".$row['token']." host=".$row['host'];
-	if($row['user'])
-		echo " username=".$row['user'];
+	if($row['username'])
+		echo " username=".$row['username'];
+	echo " ";
 }
 
 
